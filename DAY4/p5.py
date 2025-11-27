be6b9e4adb3514898447d4132e472a744e7cfe17
Palanikarthikeyan <abpalanikarthik@gmail.com>
2025-11-27 17:34:34 +0530

    Delete DAY4/p5.py

--- DAY4/p5.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-file = st.file_uploader('select your input file:')
-if file:
-    st.success(f'Input file:  {file.name} is loaded')
-    df = pd.read_csv(file)
-    st.write(df)
-    
